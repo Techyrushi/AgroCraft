bbd0d549e5b3c6d31c33d256ace09215ddf89695
Rushikesh Chavan <122292947+Techyrushi@users.noreply.github.com>
2023-09-19 21:38:01 +0530

    Add files via upload

---
+++ Includes/db.php
@@ -0,0 +1,7 @@
+<?php 
+
+        $con = mysqli_connect("sql212.infinityfree.com","if0_35053499","H33w7n4AEif0_35053499","if0_35053499_agrocraft");
+      
+        if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySql " . mysqli_connect_error();
+        }
